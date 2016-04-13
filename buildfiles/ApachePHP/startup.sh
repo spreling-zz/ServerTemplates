37d261e4b2e147a43aa03cc41f101e3edec752c8
Harm Jacob Drijfhout <harm@spreling.nl>
2016-04-13 12:01:04 +0200

    asd

--- buildfiles/ApachePHP/startup.sh
+++ buildfiles/ApachePHP/startup.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 source /etc/apache2/envvars
-tail -F /var/sites/customers/*/logs/* -F /var/log/apache2/*
+#tail -F /var/sites/customers/*/logs/* -F /var/log/apache2/*
 exec apache2 -D FOREGROUND
