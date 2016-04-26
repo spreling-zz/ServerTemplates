81fbfe06a120d295ab0988df7ba12c4db7b2a530
Harm Jacob Drijfhout <harm@spreling.nl>
2016-04-26 08:35:09 +0200

    Signed-off-by: Harm Jacob Drijfhout <harm@spreling.nl>

--- buildfiles/ApachePHP-DEV/startup.sh
+++ buildfiles/ApachePHP-DEV/startup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-chown www-data:www-data /app -R
-
+set -e
 source /etc/apache2/envvars
 
 echo "Servername $(cat /etc/hostname)" >> /etc/apache2/apache2.conf
 
+chown www-data:www-data /app -R
+
 exec apache2 -D FOREGROUND
 
-tail -F /var/sites/customers/*/logs/* -F /var/log/apache2/*
+tail -F /var/log/apache2/error.log -F /var/log/apache2/access.log
